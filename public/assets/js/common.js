0b7968f67ac4a64046ddcc9f272f5819c78551e6
mata-KI <bodhihi2011@163.com>
2019-10-29 21:33:56 +0800

    请求拦截和按回车触发登录事件

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,8 +1,10 @@
 window.addEventListener('load',function() {
     var login=document.querySelector('#login');
     login.addEventListener('click',function(){ 
+        //获取邮箱和密码
         var email=document.querySelector('#email').value;
         var password=document.querySelector('#password').value;
+        // 判空
         if(email.trim().length===0){ 
             alert('输入邮箱');
             return;
@@ -11,6 +13,7 @@ window.addEventListener('load',function() {
             alert('输入密码')
             return;
         }
+        // 发送请求
         $.ajax({
             url:'/login',
             type:'post',
@@ -25,5 +28,11 @@ window.addEventListener('load',function() {
                 alert('账户或者密码错误？？？')
             } 
         })
+    })
+    // 判断是否按下回车
+    document.body.addEventListener('keydown',function (e) {
+        if(e.keyCode==13) {
+            login.click();
+        }
     })
 })

1f7e92b2c3b4884752ba8c186d339744516c7082
mata-KI <bodhihi2011@163.com>
2019-11-03 14:05:45 +0800

    主页评论改

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -23,20 +23,20 @@ $.ajax({
     type:'get',
     url:'/posts/count',
     success:function(response) {
-        $('#postAll').html(`<strong>${$data.postCount}</strong>篇文章（<strong>2</strong>篇草稿）`)    
+        $('#postAll').html(`<strong>${response.postCount}</strong>篇文章（<strong>2</strong>篇草稿）`)    
     }
 })
 $.ajax({
     type:'get',
     url:'/categories/count',
     success:function(response) {
-        $('#cateAll').html(`<strong>${data.categoryCount}</strong>个分类`)    
+        $('#cateAll').html(`<strong>${response.categoryCount}</strong>个分类`)    
     }
 })
 $.ajax({
     type:'get',
     url:'/comments/count',
     success:function(response) {
-        $('#commitAll').html(`<strong>${data.commentCount}</strong>条评论（<strong>1</strong>条待审核）`)    
+        $('#commitAll').html(`<strong>${response.commentCount}</strong>条评论（<strong>1</strong>条待审核）`)    
     }
 })

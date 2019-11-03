b03cb520b0f44c35da07c1a6eeffb64f09fca138
mata-KI <bodhihi2011@163.com>
2019-11-03 13:51:33 +0800

    首页统计

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -18,3 +18,25 @@ window.addEventListener('load', function () {
         }
     })
 })
+
+$.ajax({
+    type:'get',
+    url:'/posts/count',
+    success:function(response) {
+        $('#postAll').html(`<strong>${$data.postCount}</strong>篇文章（<strong>2</strong>篇草稿）`)    
+    }
+})
+$.ajax({
+    type:'get',
+    url:'/categories/count',
+    success:function(response) {
+        $('#cateAll').html(`<strong>${data.categoryCount}</strong>个分类`)    
+    }
+})
+$.ajax({
+    type:'get',
+    url:'/comments/count',
+    success:function(response) {
+        $('#commitAll').html(`<strong>${data.commentCount}</strong>条评论（<strong>1</strong>条待审核）`)    
+    }
+})
